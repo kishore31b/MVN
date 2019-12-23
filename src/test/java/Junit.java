5b58d927602c1352af8f3ec636d12cdd76091c72
harindra <harindra48@gmail.com>
2019-12-23 20:05:49 +0530

    src

---
+++ src/test/java/Junit.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Junit {
+
+	public void JunitPass() {
+		int a = 1 ;
+		int b = 2 ;
+		assertTrue( a + b == 3);
+	}
+	
+}
+ 
